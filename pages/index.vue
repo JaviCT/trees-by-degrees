24e2923834e49eb6cbc23a0fb7b179caf9a5b7e1
javict <6javierct93@gmail.com>
2019-10-19 15:36:12 +0100

    added map view

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,11 @@
 <template>
 <v-layout style="height: 100%" align-content-center>
-  <p class="title1 display-4">Trees By Degrees</p>
-  <iframe class="viewer" src="https://jeromeetienne.github.io/threex.planets/examples/earth.html" frameborder="0"/>
-  <div class="selector">
-    <v-autocomplete
-      v-model="country"
-      :items="countries"
-      item-value="value"
-      item-text="name"
-      label="Country"
-      return-object
-      style="float: left"
-    />
-    <v-autocomplete
-      v-model="city"
-      label="Cities"
-      :items="cities"
-      @change="goToCity"
-    ></v-autocomplete>
+  <div class="viewer">
+    <p class="title1 display-4">Trees By Degrees</p>
+    <iframe class="viewer" src="https://jeromeetienne.github.io/threex.planets/examples/earth.html" frameborder="0"/>
+    <div class="selector">
+      <CitySelector />
+    </div>
   </div>
 </v-layout>
 </template>
@@ -41,15 +29,13 @@
 </style>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
 import cities from '~/assets/cities.json'
 import config from "@/services/data.config"
+import CitySelector from '@/components/CitySelector'
 
 export default {
   components: {
-    Logo,
-    VuetifyLogo
+    CitySelector
   },
    data: function () {
     return {
@@ -68,9 +54,7 @@ export default {
   },
   methods: {
     goToCity() {
-      console.log(this.city);
-      console.log(this.country);
-      
+      this.$router.push("/map")
     }
   }
 }
---
+++ pages/map.vue
@@ -0,0 +1,49 @@
+<template>
+<v-layout style="height: 100%" align-content-center>
+    <CitySelector class="selector" />
+  <div id="map" style="height: 100%; width: 100%"></div>
+</v-layout>
+</template>
+
+<style scoped>
+.viewer {
+  width: 100%;
+  height: 100%;
+}
+.title1 {
+  position:absolute;
+  z-index: 1;
+  left: 30%;
+}
+.selector {
+  position:absolute;
+  z-index: 1;
+  top: 5px;
+  left: 5px;
+}
+</style>
+
+<script>
+import L from "wrld.js"
+import CitySelector from '@/components/CitySelector'
+
+export default {
+  components: {
+    Map,
+    CitySelector
+  },
+   data: function () {
+    return {
+    }
+  },
+  mounted() {
+    L.map('map', "68fc6881bbfee411f30452ab9a919dcc", {
+      center: [51.517327, -0.120005],
+      zoom: 15
+    });
+
+  },
+  methods: {
+  }
+}
+</script>
